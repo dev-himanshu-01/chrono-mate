a48d140cfe125586efe1c439b3817c62e9b29842
dev-himanshu-01 <himanshu01.dev@gmail.com>
2023-04-03 06:55:53 -1200

    header :tick

---
+++ src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import "./Header.css"
+import Button from '@mui/material/Button';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import BUYMEACOFFEE from '../Assets/buymeacoffee.png'
+
+const Header = () => {
+
+    const GITHUB_REPO_LINK = 'https://github.com/dev-himanshu-01/task-master';
+
+    const BUY_ME_A_COFFEE_LINK = 'https://www.buymeacoffee.com/garadiya0';
+
+    const BUYMEACOFFEE_btn_handler = () => {
+        window.open("https://www.buymeacoffee.com/garadiya0", "_blank");
+    }
+
+    return (
+        <section className='header-section'>
+            <Button
+                variant="contained"
+                className='github-btn shineEff'
+                href={GITHUB_REPO_LINK}
+                target='_blank'
+                startIcon={<GitHubIcon />}>
+                github
+            </Button>
+
+            <div className="buymeacoffee-btn shineEff" onClick={BUYMEACOFFEE_btn_handler}>
+                <img src={BUYMEACOFFEE} height={'30'} alt="buymeacoffee-img" />
+                <span>buy me a coffee</span>
+            </div>
+
+        </section>
+    )
+}
+
+export default Header;
