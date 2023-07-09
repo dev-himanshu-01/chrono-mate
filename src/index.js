53579fd96479e0352830702bef949bbf66045c00
himanshu <himanshu01.dev@gmail.com>
2023-07-10 02:24:06 +0530

    envirnomental variable added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Auth0Provider
-      domain="task-master-tenant.us.auth0.com"
-      clientId="GQscshvfRlWD79d8OwNOLdoNTqWL2z9E"
+      domain={process.env.AUTH0_DOMAIN}
+      clientId={process.env.AUTH0_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
